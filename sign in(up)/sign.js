f7053970e5928094e58cd19dd975ce31b38b0689
naberuhinazlata <nabeuhinazlata793@gmail.com>
2025-02-24 10:05:08 +0200

    Update sign.js

--- sign in(up)/sign.js
+++ sign in(up)/sign.js
@@ -4,11 +4,11 @@ let inputPass = document.querySelector(".pass")
 eye.addEventListener("click", function() {
     if(inputPass.type === "password") {
         inputPass.type = "text"
-        eye.src = "photo_2025-02-22_11-31-36.jpg";
+        eye.src = "photo_2025-02-23 open.jpg";
     }
     else{
         inputPass.type = "password";
-        eye.src = "photo_2025-02-21_17-23-45.jpg";
+        eye.src = "photo_2025-02-23 close.jpg";
     }
 });
 
